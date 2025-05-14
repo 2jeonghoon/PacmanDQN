d95eb5ea75babbaa3ccea78ec8baab3550c37010
Karim <51705815+2jeonghoon@users.noreply.github.com>
2025-05-14 11:38:51 +0900

    Update tiny_dqn.py

--- tiny_dqn.py
+++ tiny_dqn.py
@@ -30,7 +30,11 @@ import tensorflow.compat.v1 as tf
 
 tf.disable_v2_behavior()
 
-env = gym.make("MsPacman-v0", render_mode="human")
+if(args.render):
+    env = gym.make("MsPacman-v0", render_mode="human")
+else:
+    env = gym.make("MsPacman-v0")
+    
 done = True  # env needs to be reset
 
 # First let's build the two DQNs (online & target)
@@ -159,27 +163,6 @@ with tf.Session() as sess:
     else:
         init.run()
         copy_online_to_target.run()
-        
-    if args.test:
-        obs = env.reset()
-        for skip in range(skip_start):
-            obs, reward, terminated, truncated, info = env.step(0)
-
-        state = preprocess_observation(obs)
-        done = False
-
-        while not done:
-            if args.render:
-                env.render()
-            q_values = online_q_values.eval(feed_dict={X_state: [state]})
-            action = np.argmax(q_values)
-            obs, reward, terminated, truncated, info = env.step(action)
-            done = terminated or truncated
-            state = preprocess_observation(obs)
-
-        input("테스트 플레이 종료. 창을 닫으려면 Enter 키를 누르세요.")
-        env.close()
-        exit()
 
     while True:
         step = global_step.eval()
